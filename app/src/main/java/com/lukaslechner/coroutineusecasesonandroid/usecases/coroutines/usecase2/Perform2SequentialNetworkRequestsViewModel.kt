a496515981a03a1b9b0c6659460e082d21710d2e
muthukumar <muthukumar.m@contus.in>
2026-05-10 16:50:08 +0530

    Implement sequential network requests in Perform2SequentialNetworkRequestsViewModel

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,24 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        // TODO: Exercise 1
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val androidVersions = mockApi.getRecentAndroidVersions()
+                val latestAndroidVersionFeatures = mockApi.getAndroidVersionFeatures(androidVersions.last().apiLevel)
+                uiState.value = UiState.Success(latestAndroidVersionFeatures)
+            }catch (ex: Exception){
+                uiState.value = UiState.Error("Something went wrong")
+            }
+        }
     }
 }
